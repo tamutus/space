0f19e43f040b8fb437e1d53e92660247ba3fef4e
Lavra Tamutus <lavratamutus@gmail.com>
2023-01-09 14:47:03 -0500

    Add nonbinary and intersectional flag animations

---
+++ components/animated/AnimatedFlag.vue
@@ -0,0 +1,169 @@
+<template>
+  <div class="flag-holder">
+    <div class="flag">
+      <div class="top stripe animatedWave"></div>
+      <div class="black colored stripe animatedWave"></div>
+      <div class="purple colored stripe animatedWave"></div>
+      <div class="white colored stripe animatedWave"></div>
+      <div class="yellow colored stripe animatedWave"></div>
+      <div class="bottom blank"></div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.flag-holder {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+}
+.flag {
+  height: 360px;
+  min-height: 360px;
+  width: 400px;
+  min-width: 400px;
+  text-align: center;
+  margin: 10% auto 100px auto;
+  transform: rotate(-30deg) skewX(20deg) translate(3vw, 1.5vw);
+  overflow: hidden;
+}
+@media screen and (max-width: 950px) {
+  .flag {
+  }
+}
+.stripe {
+  overflow: visible;
+  height: 17%;
+  /*   background-color: pink; */
+  padding-bottom: 0px;
+  padding-top: 0px;
+  position: relative;
+  /*   z-index: 1; */
+}
+.top.stripe {
+  height: 14%;
+  z-index: 5;
+}
+.top.stripe:before,
+.top.stripe:after {
+  content: "";
+  position: absolute;
+  right: 0;
+  bottom: -1px;
+  height: 100%;
+  background: radial-gradient(closest-side, #000, #000 50%, transparent 50%);
+  background-size: 33.3333% 100%;
+  background-position: 0px 0px;
+  background-repeat: repeat-x;
+}
+.top.stripe:after {
+  background: radial-gradient(closest-side, transparent 50%, #000 50%, #000);
+  background-size: 33.3333% 100%;
+  height: 100%;
+  background-position: 100px -25px;
+  background-repeat: repeat-x;
+  bottom: -26px;
+}
+.colored.stripe:before,
+.colored.stripe:after {
+  content: "";
+  position: absolute;
+  /* left: -200deg; */
+  height: 81.699%;
+  right: 0;
+  bottom: -81%;
+  background-image: radial-gradient(
+    closest-side,
+    var(--stripe-color),
+    var(--stripe-color) 50%,
+    transparent 50%
+  );
+  background-size: 33.3333% 100%;
+  background-position: 25% 0%;
+  background-repeat: repeat-x;
+}
+.colored.stripe:after {
+  background: radial-gradient(
+    closest-side,
+    transparent 50%,
+    var(--stripe-color) 50%,
+    var(--stripe-color)
+  );
+}
+.black.stripe {
+  height: 4.1667%;
+  --stripe-color: #000;
+  background-color: var(--stripe-color);
+  z-index: 4;
+}
+.black.stripe:before,
+.black.stripe:after {
+  bottom: -333%;
+  height: 333.3333%;
+  background-size: 200px 50px;
+  background-position: 100px 0px;
+  background-repeat: repeat-x;
+}
+.black.stripe:after {
+  height: 333.3333%;
+  background-size: 33.3333% 100%;
+  background-position: 0px 25px;
+  background-repeat: repeat-x;
+  bottom: -24px;
+}
+.purple.stripe {
+  --stripe-color: #9c59d1;
+  background-color: var(--stripe-color);
+  z-index: 3;
+}
+
+.purple.stripe:after {
+  background-size: 200px 50px;
+  height: 50px;
+  background-position: 0px 25px;
+  background-repeat: repeat-x;
+  bottom: -24px;
+}
+.white.stripe {
+  --stripe-color: white;
+  background-color: var(--stripe-color);
+  z-index: 2;
+}
+.white.stripe:after {
+  background-size: 200px 50px;
+  height: 50px;
+  background-position: 0px 25px;
+  background-repeat: repeat-x;
+  bottom: -24px;
+}
+.yellow.stripe {
+  --stripe-color: #fff430;
+  background-color: var(--stripe-color);
+  z-index: 1;
+}
+.yellow.stripe:after {
+  background-size: 200px 50px;
+  height: 50px;
+  background-position: 0px 25px;
+  background-repeat: repeat-x;
+  bottom: -24px;
+}
+.animatedWave:before,
+.animatedWave:after {
+  left: -200px;
+  animation: animateWave 2s linear infinite;
+}
+@keyframes animateWave {
+  to {
+    transform: translateX(200px);
+  }
+}
+.blank {
+  height: 50px;
+  background-color: transparent;
+}
+</style>
---
+++ components/animated/AnimatedIntersectionality.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="container">
+    <svg
+      version="1.1"
+      baseProfile="full"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 1000 600"
+      id="big-intersectional-flag"
+      ref="intersectionalFlag"
+    >
+      <rect :class="`red-stripe ${deanimated}`" height="100" width="1000" />
+      <rect
+        :class="`orange-stripe ${deanimated}`"
+        height="100"
+        width="1000"
+        y="100"
+      />
+      <rect
+        :class="`yellow-stripe ${deanimated}`"
+        height="100"
+        width="1000"
+        y="200"
+      />
+      <rect
+        :class="`green-stripe ${deanimated}`"
+        height="100"
+        width="1000"
+        y="300"
+      />
+      <rect
+        :class="`blue-stripe ${deanimated}`"
+        height="100"
+        width="1000"
+        y="400"
+      />
+      <rect
+        :class="`violet-stripe ${deanimated}`"
+        height="100"
+        width="1000"
+        y="500"
+      />
+      <polygon
+        :class="`black-triangle ${deanimated}`"
+        points="0,-150 450,300 0,750"
+      />
+      <polygon
+        :class="`brown-triangle ${deanimated}`"
+        points="0,-75 375,300 0,675"
+      />
+      <polygon
+        :class="`sky-triangle ${deanimated}`"
+        points="0,0 300,300 0,600"
+      />
+      <polygon
+        :class="`pink-triangle ${deanimated}`"
+        points="0,75 225,300 0,525"
+      />
+      <polygon
+        :class="`white-triangle ${deanimated}`"
+        points="0,150 150,300 0,450"
+      />
+    </svg>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Ref } from "vue";
+
+const deanimated: Ref<string> = ref("");
+
+useInterval(resetColors, 3500);
+function resetColors() {
+  deanimated.value = "deanimated";
+  setTimeout(() => {
+    deanimated.value = "";
+  }, 500);
+}
+</script>
+
+<style scoped>
+.container {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+}
+svg {
+  overflow: hidden;
+}
+polygon,
+rect {
+  animation-name: colorize;
+  animation-duration: 0.7s;
+  animation-fill-mode: backwards;
+  transition: fill 0.5s ease-in, filter 0.5s ease-in;
+}
+.deanimated {
+  animation: none;
+  filter: grayscale(0.8);
+}
+.red-stripe {
+  fill: rgba(254, 30, 37, 1);
+  animation-delay: 2s;
+}
+.orange-stripe {
+  fill: rgba(254, 148, 29, 1);
+  animation-delay: 1.8s;
+}
+.yellow-stripe {
+  fill: rgba(255, 255, 0, 1);
+  animation-delay: 1.6s;
+}
+.green-stripe {
+  fill: rgba(6, 189, 2, 1);
+  animation-delay: 1.4s;
+}
+.blue-stripe {
+  fill: rgba(0, 25, 150, 1);
+  animation-delay: 1.2s;
+}
+.violet-stripe {
+  fill: rgba(117, 0, 135, 1);
+  animation-delay: 1s;
+}
+.black-triangle {
+  fill: rgba(1, 1, 1, 1);
+  animation-delay: 0.8s;
+}
+.brown-triangle {
+  fill: rgba(96, 56, 20, 1);
+  animation-delay: 0.6s;
+}
+.sky-triangle {
+  fill: rgba(116, 213, 235, 1);
+  animation-delay: 0.4s;
+}
+.pink-triangle {
+  fill: rgba(254, 175, 197, 1);
+  animation-delay: 0.2s;
+}
+.white-triangle {
+  fill: rgba(255, 255, 255, 1);
+}
+@keyframes colorize {
+  0% {
+    filter: grayscale(1);
+  }
+  50% {
+    filter: brightness(1.5);
+  }
+  100% {
+    filter: none;
+  }
+}
+</style>
