ffe45b693930cf832e154f4d7e1ebf9b288493dc
Lavra Tamutus <lavratamutus@gmail.com>
2023-01-09 16:13:35 -0500

    add project config and favicon

---
+++ README.md
@@ -0,0 +1,46 @@
+# Lavra Tamutus' homepage
+
+See the site now: [LavraT.space](https://lavrat.space);
+
+Now using [Nuxt 3](https://v3.nuxtjs.org), a fullstack framework for [Vue](https://vuejs.org/).
+
+## Boilerplate from Nuxt init
+
+### Setup
+
+Make sure to install the dependencies:
+
+```bash
+## yarn
+yarn install
+
+## npm
+npm install
+
+## pnpm
+pnpm install --shamefully-hoist
+```
+
+### Development Server
+
+Start the development server on http://localhost:3000
+
+```bash
+npm run dev
+```
+
+### Production
+
+Build the application for production:
+
+```bash
+npm run build
+```
+
+Locally preview production build:
+
+```bash
+npm run preview
+```
+
+Checkout the [deployment documentation](https://v3.nuxtjs.org/guide/deploy/presets) for more information.
---
+++ nuxt.config.ts
@@ -0,0 +1,50 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  modules: [
+    [
+      "@nuxtjs/google-fonts",
+      {
+        families: {
+          Cabin: {
+            wght: [400, 700],
+            ital: [400, 700],
+          },
+          "Cinzel+Decorative": [700],
+          Comfortaa: {
+            wght: [300 + ".." + 700],
+          },
+          "Odibee+Sans": [400],
+          Ubuntu: {
+            wght: [300, 400, 500, 700],
+            ital: [300, 400, 500, 700],
+          },
+        },
+        download: true,
+      },
+    ],
+  ],
+  app: {
+    head: {
+      meta: [
+        {
+          name: "author",
+          content: "Lavra Tamutus",
+        },
+        {
+          name: "description",
+          content: "Homepage of Lavra Tamutus",
+        },
+        {
+          name: "keywords",
+          content:
+            "web,design,portfolio,designer,fullstack,full,stack,developer,node,express,nodejs,backend,frontend,programmer",
+        },
+      ],
+      link: [{ rel: "stylesheet", href: "/stylesheets/main.css" }],
+    },
+    pageTransition: {
+      name: "page",
+    },
+  },
+  components: true,
+});
