c1e23e7b19d421aa1d2401908562aac2f01067f9
Lavra Tamutus <lavratamutus@gmail.com>
2023-01-09 14:49:44 -0500

    add component for a large, responsive top margin

---
+++ components/box/BoxFocus.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="page-focus">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.page-focus {
+  margin-top: 23vh;
+}
+</style>
