064e2108041370e1dac5751ee58c25280ea53de7
Lavra Tamutus <lavratamutus@gmail.com>
2023-03-27 19:38:40 -0400

    Add Privacy Policy page

---
+++ pages/privacy-policy.vue
@@ -0,0 +1,143 @@
+<template>
+  <div id="privacy-policy-page">
+    <BackgroundTexture texture="wood">
+      <NavHeader>Privacy Policy</NavHeader>
+
+      <BoxTube>
+        <em>
+          This Privacy Policy governs the manner in which I collect, use,
+          maintain, and disclose information collected from users (each, a
+          "User") of my website (the "Site") and any related services offered by
+          me. This privacy policy applies to the Site and all products and
+          services offered by me.
+        </em>
+
+        <h2>Information Collection and Use</h2>
+
+        <p>
+          I may collect personal identification information from Users in a
+          variety of ways, including, but not limited to, when Users visit my
+          site, register on the site, place an order, fill out a form, respond
+          to a survey, and in connection with other activities, services,
+          features, or resources I make available on my Site. Users may be asked
+          for, as appropriate, name, email address, mailing address, phone
+          number, and credit card information. I will collect personal
+          identification information from Users only if they voluntarily submit
+          such information to me. Users can always refuse to supply personal
+          identification information, though that may prevent them from engaging
+          in certain Site-related activities.
+        </p>
+
+        <h2>Non-personal identification information</h2>
+
+        <p>
+          I may collect non-personal identification information about Users
+          whenever they interact with my Site. Non-personal identification
+          information may include the browser name, the type of computer, and
+          technical information about Users' means of connection to my Site,
+          such as the operating system and the Internet service providers
+          utilized and other similar information.
+        </p>
+
+        <h2>Web browser cookies</h2>
+
+        <p>
+          My Site may use "cookies" to enhance the User experience. Users' web
+          browsers place cookies on their hard drive for record-keeping purposes
+          and sometimes to track information about them. Users may choose to set
+          their web browser to refuse cookies or to alert them when cookies are
+          being sent. If they do so, note that some parts of the Site may not
+          function properly.
+        </p>
+
+        <h2>How I use collected information</h2>
+
+        <p>
+          I collect and use Users' personal information for the following
+          purposes:
+        </p>
+
+        <ul>
+          <li>To improve customer service</li>
+          <li>To personalize user experience</li>
+          <li>To process payments</li>
+          <li>To send periodic emails</li>
+          <li>To respond to inquiries and customer support requests</li>
+          <li>To run promotions, contests, surveys, or other Site features</li>
+          <li>To comply with applicable laws and regulations</li>
+        </ul>
+
+        <h2>How I protect your information</h2>
+        <p>
+          I adopt appropriate data collection, storage, and processing practices
+          and security measures to protect against unauthorized access,
+          alteration, disclosure, or destruction of your personal information,
+          username, password, transaction information, and data stored on my
+          Site. I continually research how to improve the security of my web
+          infrastructure. I review and assess known security flaws in libraries
+          I may use before integrating them. Up-to-date security measures are of
+          paramount importance to me, and I act promptly to address any security
+          incidents or known exploits.
+        </p>
+
+        <h2>Sharing your personal information</h2>
+
+        <p>
+          I do not sell, trade, or rent Users' personal identification
+          information to others. I may share generic aggregated demographic
+          information not linked to any personal identification information
+          regarding visitors and users with my business partners, trusted
+          affiliates, and advertisers for the purposes outlined above.
+        </p>
+
+        <h2>Third-party websites</h2>
+
+        <p>
+          Users may find advertising or other content on my Site that links to
+          the sites and services of my partners, suppliers, advertisers,
+          sponsors, licensors, and other third parties. I do not control the
+          content or links that appear on these sites and are not responsible
+          for the practices employed by websites linked to or from my Site. In
+          addition, these sites or services, including their content and links,
+          may be constantly changing. These sites and services may have their
+          own privacy policies and customer service policies. Browsing and
+          interaction on any other website, including websites that have a link
+          to my Site, is subject to that website's own terms and policies.
+        </p>
+
+        <h2>Changes to this privacy policy</h2>
+
+        <p>
+          I have the discretion to update this privacy policy at any time. When
+          I do, I will post a notification on the main page of my Site and
+          revise the updated date at the bottom of this page. I encourage Users
+          to frequently check this page for any changes to stay informed about
+          how I are helping to protect the personal information I collect. You
+          acknowledge and agree that it is your responsibility to review
+        </p>
+        <div class="bite-size">
+          <BoxAlmond flavor="cool"
+            ><em>Last updated: March 27, 2023</em></BoxAlmond
+          >
+        </div></BoxTube
+      >
+    </BackgroundTexture>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+h2 {
+  margin: 3rem auto 1.5rem auto;
+}
+li {
+  margin-top: 0rem;
+  margin-bottom: 0;
+}
+.bite-size {
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+}
+</style>
